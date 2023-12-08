7a09bf7786fec5e917e5ada09e9d52a9d4dda440
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-08 19:13:19 +0300

    Complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,55 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-3xl font-semibold my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          className="rounded-full w-24 object-cover
+          self-center
+          mt-2
+          hover:cursor-pointer
+          "
+          src={currentUser.avatar}
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button
+          className="bg-slate-700 
+        p-3
+        rounded-lg
+        uppercase
+        text-white
+        hover:opacity-90
+        disabled:opacity-80
+        "
+        >
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
