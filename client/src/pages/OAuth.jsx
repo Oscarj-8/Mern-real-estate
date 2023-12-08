f812d7fd3ef2eb009d066f1186ddaacf821bfd41
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-08 15:51:52 +0300

    Create google oauth api

--- client/src/pages/OAuth.jsx
+++ client/src/pages/OAuth.jsx
@@ -1,7 +1,11 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice.js";
 
 export default function OAuth() {
+  const dispatch = useDispatch();
+
   const handleGoogleaClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -9,6 +13,19 @@ export default function OAuth() {
 
       const result = await signInWithPopup(auth, provider);
 
+      const res = fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
       console.log(result);
     } catch (error) {
       console.log("Could not sign in with Google");
