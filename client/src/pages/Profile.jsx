e3189b3ba2d428c4a0bf1c53c28921d839daa25b
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-27 19:00:34 +0300

    Complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -139,6 +139,26 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        console.log(error.message);
+      }
+
+      setUserListing((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-center text-3xl font-semibold my-7">Profile</h1>
@@ -261,7 +281,12 @@ export default function Profile() {
               </Link>
               <div className="flex flex-col">
                 <button className="text-green-700 uppercase">Edit</button>
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
               </div>
             </div>
           ))}
