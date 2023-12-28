95eed4ea35beaf2e46fc5c16615a1661fa0d5170
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-29 01:48:51 +0300

    Start fetching listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 export default function Listing() {
-  return <div>Listing</div>;
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading();
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(error.message);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  });
+
+  return <div>{listing && listing.name}</div>;
 }
