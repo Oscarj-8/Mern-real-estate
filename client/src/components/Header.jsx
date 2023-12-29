bd7e69d2c9d8442fa64739844bc46f1d060f61a4
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-29 23:45:22 +0300

    Start adding search func in the UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -22,6 +23,7 @@ export default function Header() {
             className="bg-transparent focus:outline-none
             w-24 sm:w-64
             "
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <FaSearch className="text-slate-600" />
         </form>
