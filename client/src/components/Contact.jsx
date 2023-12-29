2a3d84a4375032664a5bdded260ea1ae6cb56dea
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-29 22:05:50 +0300

    Fix commented area

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,84 +1,30 @@
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// export default function Contact({ listing }) {
-//   const [landLord, setLandLord] = useState(null);
-//   const [message, setMessage] = useState("");
-//   const onChange = (e) => {
-//     setMessage(e.target.value);
-//   };
-//   useEffect(() => {
-//     const fetchLandLord = async () => {
-//       try {
-//         const res = await fetch(`/api/user/${listing.userRef}`);
-//         const data = await res.json();
-//         setLandLord(data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     fetchLandLord();
-//   }, [listing.userRef]);
-
-//   return (
-//     <>
-//       {landLord && (
-//         <div className="flex flex-col gap-2">
-//           <p>
-//             Contact: <span className="font-semibold">{landLord.username}</span>{" "}
-//             for{" "}
-//             <span className="font-semibold">{listing.name.toLowerCase()}</span>
-//           </p>
-//           <textarea
-//             name="message"
-//             id="message"
-//             rows="2"
-//             value={message}
-//             onChange={onChange}
-//             className="w-full border p-3 rounded-lg"
-//             placeholder="Enter your message here"
-//           ></textarea>
-//           <Link
-//             to={`mailto:${landLord.email}?subject=Regarding ${
-//               listing.name
-//             }&body=${encodeURIComponent(message)}`}
-//             className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
-//           >
-//             Send Message
-//           </Link>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 export default function Contact({ listing }) {
-  const [landlord, setLandlord] = useState(null);
+  const [landLord, setLandLord] = useState(null);
   const [message, setMessage] = useState("");
   const onChange = (e) => {
     setMessage(e.target.value);
   };
-
   useEffect(() => {
-    const fetchLandlord = async () => {
+    const fetchLandLord = async () => {
       try {
         const res = await fetch(`/api/user/${listing.userRef}`);
         const data = await res.json();
-        setLandlord(data);
+        setLandLord(data);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchLandlord();
+    fetchLandLord();
   }, [listing.userRef]);
+
   return (
     <>
-      {landlord && (
+      {landLord && (
         <div className="flex flex-col gap-2">
           <p>
-            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            Contact: <span className="font-semibold">{landLord.username}</span>{" "}
             for{" "}
             <span className="font-semibold">{listing.name.toLowerCase()}</span>
           </p>
@@ -88,12 +34,13 @@ export default function Contact({ listing }) {
             rows="2"
             value={message}
             onChange={onChange}
-            placeholder="Enter your message here..."
             className="w-full border p-3 rounded-lg"
+            placeholder="Enter your message here"
           ></textarea>
-
           <Link
-            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            to={`mailto:${landLord.email}?subject=Regarding ${
+              listing.name
+            }&body=${encodeURIComponent(message)}`}
             className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
           >
             Send Message
