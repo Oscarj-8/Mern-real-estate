d39cc985184fe08d11fb5bb70e85bac4d44e002b
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-30 23:36:06 +0300

    Create listing item component and show all listings

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,3 +1,48 @@
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
 export default function ListingItem({ listing }) {
-  return <div>{listing.regularPrice}</div>;
+  return (
+    <div className="bg-white shadow-md hover:shadow-lg overflow-hidden rounded-lg w-full sm:w-[300px]">
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={listing.imageUrls[0]}
+          alt="listing cover"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-500"
+        />
+        <div className="p-3 gap-2 flex flex-col w-full">
+          <p className="text-lg font-semibold text-slate-700 truncate">
+            {listing.name}
+          </p>
+          <div className="flex items-center gap-1">
+            <MdLocationOn className="w-4 h-4 text-green-700" />
+            <p className="truncate text-sm text-gray-600 w-full">
+              {listing.address}
+            </p>
+          </div>
+          <p className="line-clamp-2 text-sm text-gray-600">
+            {listing.description}
+          </p>
+          <p className="text-slate-500 mt-2 font-semibold">
+            $
+            {listing.offer
+              ? listing.discountedPrice.toLocaleString()
+              : listing.regularPrice.toLocaleString()}
+            {listing.type === "rent" && " / month"}
+          </p>
+          <div className="flex gap-3 text-slate-700">
+            <div className="font-bold text-xs">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds`
+                : `${listing.bedrooms} bed`}
+            </div>
+            <div className="font-bold text-xs">
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} baths`
+                : `${listing.bathrooms} bath`}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 }
