1547a34e067f64544032a03cd5e690b06e4f5afb
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-31 17:40:37 +0300

    Fix width issue on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -90,9 +90,9 @@ export default function Home() {
       </Swiper>
 
       {/*listing result*/}
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+      <div className="max-w-7xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
-          <div>
+          <div className="w-full">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent Offers
@@ -104,7 +104,7 @@ export default function Home() {
                 Show more offers
               </Link>
             </div>
-            <div className="flex flex-wrap gap-4">
+            <div className="flex flex-wrap gap-4 ">
               {offerListings.map((listing) => (
                 <ListingItem listing={listing} key={listing._id} />
               ))}
